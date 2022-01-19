ad8b08e97ee483aedca68e8e044dd6e99e8ed231
JustAnOkapi <39418801+JustAnOkapi@users.noreply.github.com>
2022-01-18 21:03:40 -0600

    init

---
+++ mainPY.py
@@ -0,0 +1,17 @@
+import numpy as np
+from browser import ajax
+from browser import document as doc
+from browser import window
+from url import link
+
+window.console.clear()
+
+
+
+
+def run():
+    doc["preloader"].attrs["style"] = f"display: none;"
+
+
+if __name__ == 'main':
+    run()
